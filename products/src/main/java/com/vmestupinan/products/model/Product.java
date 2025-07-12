34a04f58d8afef069f9cf3e5af9fbf486a5031e5
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-07-12 16:08:12 -0500

    Add Product entity and Status enum

---
+++ products/src/main/java/com/vmestupinan/products/model/Product.java
@@ -0,0 +1,32 @@
+package com.vmestupinan.products.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String category;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+}
---
+++ products/src/main/java/com/vmestupinan/products/model/Status.java
@@ -0,0 +1,6 @@
+package com.vmestupinan.products.model;
+
+public enum Status {
+    AVAILABLE,
+    UNAVAILABLE
+}
