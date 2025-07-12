ddb1ca9ac43f0fec0f39681cdae534e582a006bd
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-07-12 18:06:28 -0500

    include user role in JWT claims

--- auth/src/main/java/com/vmestupinan/auth/service/JwtService.java
+++ auth/src/main/java/com/vmestupinan/auth/service/JwtService.java
@@ -8,6 +8,7 @@ import java.util.function.Function;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -34,7 +35,12 @@ public class JwtService {
     }
 
     public String generateToken(UserDetails userDetails) {
-        return generateToken(new HashMap<>(), userDetails);
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", userDetails.getAuthorities().stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElse("ROLE_USER"));
+        return generateToken(claims, userDetails);
     }
 
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
