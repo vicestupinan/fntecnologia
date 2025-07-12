ee1eb669f9c2855899772850eb87f942a76c7334
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-07-12 12:10:07 -0500

    Add UserDetailsServiceImpl to support login by email

--- auth/src/main/java/com/vmestupinan/auth/repository/UserRepository.java
+++ auth/src/main/java/com/vmestupinan/auth/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.vmestupinan.auth.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vmestupinan.auth.model.User;
 
 public interface UserRepository extends JpaRepository <User, Long>{
-    
+    Optional<User> findByEmail(String email);
 }
---
+++ auth/src/main/java/com/vmestupinan/auth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.vmestupinan.auth.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.vmestupinan.auth.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+    
+}
