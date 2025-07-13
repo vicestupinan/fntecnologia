29e9f85ef73aaf63c4a7174c8e1ed407fde88f64
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-07-12 20:22:11 -0500

    initialize React SPA with Vite

---
+++ fnt-spa/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Box, Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box p={4}>
+      <Heading>Products</Heading>
+    </Box>
+  );
+}
+
+export default App;
