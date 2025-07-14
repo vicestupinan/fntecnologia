9a5475f6aaba7210c473f3c04369a6bea6405ae2
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-07-13 21:09:16 -0500

    update flyway migrations

--- auth/src/main/resources/db/migration/V2__insert_users.sql
+++ auth/src/main/resources/db/migration/V2__insert_users.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (name, email, password, role)
 VALUES 
-  ('User Normal', 'user@example.com', '$2a$10$A6cLG7kFWWK/fSOpAlUS1OoND2R7m9lV7kjqLPYvu/OoM8fLnoKl2', 'USER'),
-  ('Admin Root', 'admin@example.com', '$2a$10$ZUL.qfCqsvuYtNdGmniqF.DfhEtDQPL5hu1F8tly0pm4HP0kE9p4S', 'ADMIN');
+  ('User Normal', 'user@example.com', '$2a$10$9BzU69TKmqhGVa2lDmxUw.sFgxN1/wl9ms9elXnP4HoU.i.s6i.cK', 'USER'),
+  ('Admin Root', 'admin@example.com', '$2a$10$XUKNQ3c6rv0/xkjLl9PH5uPaaoI/BWnxzDTqD915BcgiKi7Mz59vu', 'ADMIN');
